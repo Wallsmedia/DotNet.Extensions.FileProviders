e0b709c26df9c504c0650a34b1eab7d3ce338aaf
AlexP <paskhin@hotmail.co.uk>
2017-07-23 11:12:06 +0100

    Publish version 2.0

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DotNet.Extensions.FileProviders
 Net Core String File Provider
+
+https://www.nuget.org/packages/DotNet.Extensions.FileProviders/
+
+```
+public static IConfigurationRoot CreateJsonConfiguration(StringFileProvider fileProvider)
+        {
+            IConfigurationRoot result;
+            var configBuilder = new ConfigurationBuilder();
+            configBuilder.AddJsonFile(fileProvider, "string", false, false);
+            result = configBuilder.Build();
+            return result;
+        }
+```
---
+++ src/DotNet.Extensions.FileProviders/StringFileProvider.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Text;
+using System.Collections;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Primitives;
+
+namespace DotNet.Extensions.FileProviders
+{
+
+    public class StringFileProvider : IFileProvider, IFileInfo, IDirectoryContents
+    {
+        string _inputContext;
+        List<IFileInfo> list;
+        public StringFileProvider(string inputContext)
+        {
+            _inputContext = inputContext;
+            list = new List<IFileInfo>();
+            list.Add(this);
+        }
+
+        // IFileInfo
+        public bool Exists => true;
+
+        public bool IsDirectory => false;
+
+        public DateTimeOffset LastModified => DateTimeOffset.Now;
+
+        public long Length => _inputContext.Length;
+
+        public string Name => nameof(StringFileProvider);
+
+        public string PhysicalPath => "";
+
+        public Stream CreateReadStream()
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(_inputContext));
+        }
+
+        // IFileProvider
+        public IDirectoryContents GetDirectoryContents(string subpath)
+        {
+            return this;
+        }
+
+        public IEnumerator<IFileInfo> GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        public IFileInfo GetFileInfo(string subpath)
+        {
+            return this;
+        }
+           
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return list.GetEnumerator();
+        }
+
+        IChangeToken IFileProvider.Watch(string filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
